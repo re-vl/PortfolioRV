11692fc834ea414e5d0447ac26f2ccb5db1640e1
Vladimir <rva7171@gmail.com>
2021-03-23 07:18:37 +0300

    модальное окно и typing заголовка

---
+++ src/js/script.js
@@ -0,0 +1,41 @@
+window.addEventListener("DOMContentLoaded", () => {
+   const menu = document.querySelector(".nav");
+
+   document.onscroll = function () {
+      if (document.documentElement.scrollTop > 300) {
+         menu.classList.remove("moveup");
+         menu.classList.add("movedowd");
+         menu.classList.remove("hide");
+      } else {
+         menu.classList.remove("movedowd");
+         menu.classList.add("moveup");
+      }
+   };
+
+   const text = document.getElementById("typed"),
+      txt = "Речмедилов Владимир",
+      speed1 = 200,
+      speed2 = 30;
+   var i = 0,
+      backspace = false;
+
+   function typeWriter() {
+      text.innerHTML = txt.substr(0, i);
+
+      if (i > txt.length) {
+         backspace = true;
+      }
+      if (i == 0) {
+         backspace = false;
+      }
+      if (!backspace) {
+         i++;
+         setTimeout(typeWriter, speed1);
+      } else {
+         i--;
+         setTimeout(typeWriter, speed2);
+      }
+   }
+
+   typeWriter();
+});
